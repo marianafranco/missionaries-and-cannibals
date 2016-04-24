6715576218ee216a72af7c4a4aa3b247fd9dcd7f
Mariana Ramos Franco <ramos.franco@gmail.com>
2016-04-24 02:43:36 +0200

    Using default list type for FIFO queue and fixing state comparison by adding eq and hash methods

--- python/missionaries_and_cannibals.py
+++ python/missionaries_and_cannibals.py
@@ -1,45 +1,6 @@
 import math
 
 #______________________________________________________________________________
-# Queues: FIFOQueue (source: http://aima-python.googlecode.com/svn/trunk/utils.py)
-
-class Queue:
-    """Queue is an abstract class/interface.
-    Supports the following methods and functions:
-        q.append(item)  -- add an item to the queue
-        q.extend(items) -- equivalent to: for item in items: q.append(item)
-        q.pop()         -- return the top item from the queue
-        len(q)          -- number of items in q (also q.__len())
-        item in q       -- does q contain item?
-    If Python ever gets interfaces, Queue will be an interface."""
-
-    def __init__(self):
-        abstract
-
-    def extend(self, items):
-        for item in items: self.append(item)
-
-class FIFOQueue(Queue):
-    """A First-In-First-Out Queue."""
-    def __init__(self):
-        self.A = []; self.start = 0
-    def append(self, item):
-        self.A.append(item)
-    def __len__(self):
-        return len(self.A) - self.start
-    def extend(self, items):
-        self.A.extend(items)
-    def pop(self):
-        e = self.A[self.start]
-        self.start += 1
-        if self.start > 5 and self.start > len(self.A)/2:
-            self.A = self.A[self.start:]
-            self.start = 0
-        return e
-    def __contains__(self, item):
-        return item in self.A[self.start:]
-
-#______________________________________________________________________________
 # Missionaries and Cannibals Problem
 
 class State():
@@ -65,6 +26,14 @@ class State():
 			return True
 		else:
 			return False
+
+	def __eq__(self, other):
+		return self.cannibalLeft == other.cannibalLeft and self.missionaryLeft == other.missionaryLeft \
+                   and self.boat == other.boat and self.cannibalRight == other.cannibalRight \
+                   and self.missionaryRight == other.missionaryRight
+
+	def __hash__(self):
+		return hash((self.cannibalLeft, self.missionaryLeft, self.boat, self.cannibalRight, self.missionaryRight))
 
 def successors(cur_state):
 	children = [];
@@ -136,11 +105,11 @@ def breadth_first_search():
 	initial_state = State(3,3,'left',0,0)
 	if initial_state.is_goal():
 		return initial_state
-	frontier = FIFOQueue()
+	frontier = list()
 	explored = set()
 	frontier.append(initial_state)
 	while frontier:
-		state = frontier.pop()
+		state = frontier.pop(0)
 		if state.is_goal():
 			return state
 		explored.add(state)
@@ -169,7 +138,7 @@ def main():
 	print "Missionaries and Cannibals solution:"
 	print "(cannibalLeft,missionaryLeft,boat,cannibalRight,missionaryRight)"
 	print_solution(solution)
-	
+
 # if called from the command line, call main()
 if __name__ == "__main__":
     main()
